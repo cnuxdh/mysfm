7108e4a4ecb91492ee8e3ab2f36be01816c0d1a2
cnuxdh <6121@cnu.edu.cn>
2016-09-09 18:37:02 +0800

    dll output definition

--- export.hpp
+++ export.hpp
@@ -4,9 +4,13 @@
 #define CV_EXPORT_H
 
 
+#ifdef _WIN32
+# define DLL_EXPORT __declspec( dllexport )
+#else
+# define DLL_EXPORT
+#endif
 
-
-#define DLL_EXPORT  _declspec(dllexport)
+//#define DLL_EXPORT  _declspec(dllexport)
 
 
 
